a7ded3651fdb5c6b57fd9f8c6f569cdd8f6f79c8
alain.plana <alain.plana@free-now.com>
2021-09-28 07:44:21 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FunctionComponent, useState } from 'react';
+import data from './data'
 
-function App() {
+const App: FunctionComponent = () => {
+  const initialText: string[] = [];
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState(initialText);
+
+  const handleSubmit = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    let amount: number = (count <= 0) ? 1 : (count > 8) ? 8 : count;
+    setText(data.slice(0, amount));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section-center">
+      <h3>Do you want to genererate Lorem Ipsum... ?</h3>
+      <form onSubmit={handleSubmit} className="lorem-form">
+        <label htmlFor="amount">
+          paragraphs:
+        </label>
+        <input type="number" name="amount" id="amount" value={count} onChange={(e) => setCount(+e.target.value)} />
+        <button type="submit" className="btn">generate</button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>
+        })}
+      </article>
+    </section>
   );
 }
 
